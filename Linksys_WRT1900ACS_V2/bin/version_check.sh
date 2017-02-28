67b776f5152940d48a7dcf1f6c045ebb3f4f62ce
PatchyKnowledge <i.kotenko@sirinsoftware.com>
2017-02-28 18:29:33 +0200

    update on config backups

--- Linksys_WRT1900ACS_V2/bin/version_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh                                                                                                               
-                                                                                                                        
-get_sabai_version() {                                                                                                   
-  echo $(uci get sabai.general.prev_version)                                                                            
-}                                                                                                                       
-                                                                                                                        
-get_openwrt_version_build() {                                                                                           
-  echo $(cat /etc/sabai/sabaiopen_version_prev)                                                                         
-}                                                                                                                       
--- Linksys_WRT1900ACS_V2/files/sbin/first_boot.sh
+++ Linksys_WRT1900ACS_V2/files/sbin/first_boot.sh
@@ -3,7 +3,10 @@ if ! fw_printenv "update_done" 1>/dev/null 2>&1 &&
    ! fw_printenv "revert_enabled" 1>/dev/null 2>&1 ; then                                                               
   fw_setenv update_done 0                                                                                               
   fw_setenv revert_enabled 0                                                                                            
-  fw_setenv first_boot 1                                                                                                
+  fw_setenv first_boot 1       
+
+  /sbin/merge_configs.sh                                                                                         
+}
 else                                                                                                                    
   fw_setenv first_boot 0                                                                                                
 fi 
